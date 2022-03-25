67510549542dca9cb61e855ad5ef5828d347fa6d
hanwei <backendcloud@gmail.com>
2022-03-25 15:21:36 +0800

    delete .mvn, add js ws

---
+++ ws/js/client.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws')
+const ws = new WebSocket('ws://localhost:3000')
+
+// 接受
+ws.on('message', (message) => {
+  console.log(message)
+
+    // 当数字达到 10 时，断开连接
+    if (message == 10) {
+      ws.send('close');
+      ws.close()
+  }
+})
